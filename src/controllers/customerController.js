733a92254a744eb4bbda9c8d630c041d0f10b0c7
root <root@ip-172-31-6-20.us-east-2.compute.internal>
2023-05-17 15:08:36 +0000

    Diego valdez

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,19 +1,19 @@
 import { pool } from "../db.js";
 
 export const renderCustomers = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM customer");
+  const [rows] = await pool.query("SELECT * FROM  product");
   res.render("customers", { customers: rows });
 };
 
 export const createCustomers = async (req, res) => {
   const newCustomer = req.body;
-  await pool.query("INSERT INTO customer set ?", [newCustomer]);
+  await pool.query("INSERT INTO product  set ?", [newCustomer]);
   res.redirect("/");
 };
 
 export const editCustomer = async (req, res) => {
   const { id } = req.params;
-  const [result] = await pool.query("SELECT * FROM customer WHERE id = ?", [
+  const [result] = await pool.query("SELECT * FROM product  WHERE id = ?", [
     id,
   ]);
   res.render("customers_edit", { customer: result[0] });
@@ -22,15 +22,15 @@ export const editCustomer = async (req, res) => {
 export const updateCustomer = async (req, res) => {
   const { id } = req.params;
   const newCustomer = req.body;
-  await pool.query("UPDATE customer set ? WHERE id = ?", [newCustomer, id]);
+  await pool.query("UPDATE product set ? WHERE id = ?", [newCustomer, id]);
   res.redirect("/");
 };
 
 export const deleteCustomer = async (req, res) => {
   const { id } = req.params;
-  const result = await pool.query("DELETE FROM customer WHERE id = ?", [id]);
+  const result = await pool.query("DELETE FROM product  WHERE id = ?", [id]);
   if (result.affectedRows === 1) {
-    res.json({ message: "Customer deleted" });
+    res.json({ message: "Product deleted" });
   }
   res.redirect("/");
 };
